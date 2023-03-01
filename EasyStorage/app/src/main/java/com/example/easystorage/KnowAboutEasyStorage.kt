b198dc7cc9d793daa2edc64479c48f3097b25c9c
KishanRathod17 <rathodkishan17.kjr@gmail.com>
2023-03-01 23:49:43 +0530

    First Commit

--- EasyStorage/app/src/main/java/com/example/easystorage/KnowAboutEasyStorage.kt
+++ EasyStorage/app/src/main/java/com/example/easystorage/KnowAboutEasyStorage.kt
@@ -1,11 +1,21 @@
 package com.example.easystorage
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class KnowAboutEasyStorage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_know_about_easy_storage)
+    }
+
+    fun openActivityHowItWorks(v: View)
+    {
+        Toast.makeText(this,"You will get to know how Easy Storage works!", Toast.LENGTH_SHORT).show()
+        val intent= Intent(this,HowItWorks::class.java)
+        startActivity(intent)
     }
 }
