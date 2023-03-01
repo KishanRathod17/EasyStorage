ac9f3c847907269f4f48d3b8872f9e29d20bbe3d
KishanRathod17 <rathodkishan17.kjr@gmail.com>
2023-03-01 22:16:05 +0530

    Create KnowAboutEasyStorage.kt

---
+++ EasyStorage/app/src/main/java/com/example/easystorage/KnowAboutEasyStorage.kt
@@ -0,0 +1,11 @@
+package com.example.easystorage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class KnowAboutEasyStorage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_know_about_easy_storage)
+    }
+}
