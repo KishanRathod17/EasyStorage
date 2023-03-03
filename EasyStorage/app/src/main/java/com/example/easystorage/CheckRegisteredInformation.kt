05370252b96a315a05dcb68398b7b3d6ec71ce45
KishanRathod17 <rathodkishan17.kjr@gmail.com>
2023-03-03 10:44:19 +0530

    Register Frontend Done

--- EasyStorage/app/src/main/java/com/example/easystorage/CheckRegisteredInformation.kt
+++ EasyStorage/app/src/main/java/com/example/easystorage/CheckRegisteredInformation.kt
@@ -3,56 +3,60 @@ package com.example.easystorage
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.speech.tts.TextToSpeech
 import android.view.View
 import android.widget.TextView
 import android.widget.Toast
+import org.w3c.dom.Text
 
-class RegisterActivity : AppCompatActivity() {
-
-
-
+class CheckRegisteredInformation : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        setContentView(R.layout.activity_check_registered_information)
+
 
         val text_username = intent.getStringExtra("user_name")
-        var p_name=findViewById<TextView>(R.id.input_username)
+        var p_name=findViewById<TextView>(R.id.previous_name)
         p_name.text=text_username
 
         val text_address = intent.getStringExtra("user_address")
-        var p_address=findViewById<TextView>(R.id.input_address)
+        var p_address=findViewById<TextView>(R.id.previous_address)
         p_address.text=text_address
 
         val text_email = intent.getStringExtra("user_email")
-        var p_email=findViewById<TextView>(R.id.input_email)
+        var p_email=findViewById<TextView>(R.id.previous_email)
         p_email.text=text_email
 
         val text_mobile = intent.getStringExtra("user_mobile")
-        var p_mobile=findViewById<TextView>(R.id.input_phone)
+        var p_mobile=findViewById<TextView>(R.id.previous_mobile)
         p_mobile.text=text_mobile
-
     }
 
     fun goToMainPage(v: View)
     {
-        Toast.makeText(this,"Is all information correct?", Toast.LENGTH_SHORT).show()
-        val intent= Intent(this,CheckRegisteredInformation::class.java)
+        Toast.makeText(this,"Congrats for creating a new account", Toast.LENGTH_LONG).show()
+        val intent= Intent(this,MainActivity::class.java)
+        startActivity(intent)
+    }
 
-        //For sending data to another layout
-        val in_username=findViewById<TextView>(R.id.input_username)
+    fun goToRegisterPage(v: View)
+    {
+        Toast.makeText(this,"Please correct your mistakes",Toast.LENGTH_LONG).show()
+        val intent= Intent(this,RegisterActivity::class.java)
+
+        //For sending data to agagin to register layout
+        val in_username=findViewById<TextView>(R.id.previous_name)
         val text_username=in_username.text.toString()
         intent.putExtra("user_name",text_username)
 
-        val in_address=findViewById<TextView>(R.id.input_address)
+        val in_address=findViewById<TextView>(R.id.previous_address)
         val text_useraddress=in_address.text.toString()
         intent.putExtra("user_address",text_useraddress)
 
-        val in_email=findViewById<TextView>(R.id.input_email)
+        val in_email=findViewById<TextView>(R.id.previous_email)
         val text_useremail=in_email.text.toString()
         intent.putExtra("user_email",text_useremail)
 
-        val in_mobile=findViewById<TextView>(R.id.input_phone)
+        val in_mobile=findViewById<TextView>(R.id.previous_mobile)
         val text_usermobile=in_mobile.text.toString()
         intent.putExtra("user_mobile",text_usermobile)
 
@@ -60,4 +64,3 @@ class RegisterActivity : AppCompatActivity() {
         startActivity(intent)
     }
 }
-
