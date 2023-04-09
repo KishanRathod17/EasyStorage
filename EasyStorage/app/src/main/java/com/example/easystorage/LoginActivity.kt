207ad074a12b812d24d2ef24ed33e73f73394246
KishanRathod17 <rathodkishan17.kjr@gmail.com>
2023-04-09 11:58:39 +0530

    Forgot Password Done

--- EasyStorage/app/src/main/java/com/example/easystorage/LoginActivity.kt
+++ EasyStorage/app/src/main/java/com/example/easystorage/LoginActivity.kt
@@ -4,8 +4,10 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.core.content.ContextCompat.startActivity
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -30,6 +32,8 @@ class LoginActivity : AppCompatActivity() {
 
 
     }
+
+
     fun loginUser(v:View)
     {
         val in_email = findViewById<TextView>(R.id.login_input_email)
@@ -63,5 +67,52 @@ class LoginActivity : AppCompatActivity() {
                 }
         }
     }
+
+
+    fun onForgotPasswordClick(view: View) {
+
+        //Creating a alert box
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Forgot Password")
+        builder.setMessage("Enter your email address to receive a password reset link (Check spam if not received in inbox) ")
+
+        val input = EditText(this)
+        builder.setView(input)
+
+        builder.setPositiveButton("Send") { dialog, _ ->
+            val email = input.text.toString()
+
+            if (email.isEmpty()) {
+                Toast.makeText(this, "Please enter your email address", Toast.LENGTH_LONG).show()
+            } else {
+                val auth = Firebase.auth
+
+                //This will send the reset email
+                auth.sendPasswordResetEmail(email)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            Toast.makeText(
+                                this,
+                                "Email sent successfully $email",
+                                Toast.LENGTH_LONG
+                            ).show()
+                        } else {
+                            Toast.makeText(
+                                this,
+                                "Failed to send password reset email as ${task.exception?.message}",
+                                Toast.LENGTH_LONG
+                            ).show()
+                        }
+                    }
+            }
+        }
+
+        builder.setNegativeButton("Cancel") { dialog, _ ->
+            dialog.cancel()
+        }
+
+        builder.show()
+    }
+
 
 }
