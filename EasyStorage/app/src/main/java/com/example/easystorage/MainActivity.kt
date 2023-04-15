6eae7d5a88b754623ba253070e5d21a6b68acb60
KishanRathod17 <rathodkishan17.kjr@gmail.com>
2023-04-15 12:38:03 +0530

    DONE

--- EasyStorage/app/src/main/java/com/example/easystorage/MainActivity.kt
+++ EasyStorage/app/src/main/java/com/example/easystorage/MainActivity.kt
@@ -15,7 +15,6 @@ import com.google.firebase.ktx.Firebase
 class MainActivity : AppCompatActivity()
 {
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
